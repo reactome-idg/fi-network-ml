17701bff089ac527085f95c441690da3404b92ff
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2019-07-25 12:11:22 -0400

    New methods to get data

--- src/main/java/org/reactome/idg/dao/GeneDAO.java
+++ src/main/java/org/reactome/idg/dao/GeneDAO.java
@@ -1,12 +1,16 @@
 package org.reactome.idg.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import org.reactome.idg.model.Gene;
 
 public interface GeneDAO
 {
 	public Long addGene(String symbol);
+	public void addGenes(List<String> symbols);
 	public List<Gene> getGene(String symbol);
 	public List<Gene> getGene(Long id);
+	Map<String, Long> getSymbolToIdMapping();
+	List<Gene> getAllGenes();
 }
--- src/main/java/org/reactome/idg/dao/GeneDAOImpl.java
+++ src/main/java/org/reactome/idg/dao/GeneDAOImpl.java
@@ -1,18 +1,27 @@
 package org.reactome.idg.dao;
 
 
+import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.hibernate.CacheMode;
+import org.hibernate.FlushMode;
+import org.hibernate.JDBCException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.exception.ConstraintViolationException;
 import org.reactome.idg.model.Gene;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
 public class GeneDAOImpl implements GeneDAO
 {
-//	private static final Logger logger = LogManager.getLogger();
+	private static final Logger logger = LogManager.getLogger();
 
 	// The symbol-to-ID map is a cache intended to speed up the process of creating bulk-load files.
 	// The bulk-load files will contain on each row: two gene IDs, and a correlation value.
@@ -95,6 +104,76 @@ public class GeneDAOImpl implements GeneDAO
 									.getResultList();
 		
 		return results;
+	}
+
+	@Transactional(readOnly = true)
+	@Override
+	public Map<String, Long> getSymbolToIdMapping()
+	{
+		getSession();
+		Map<String, Long> symbolsToIds = new HashMap<>();
+		
+		@SuppressWarnings("unchecked")
+		List<Gene> genes = session.createQuery("from Gene").getResultList();
+		for (Gene g : genes)
+		{
+			symbolsToIds.put(g.getSymbol(), g.getId());
+		}
+		
+		return symbolsToIds;
+	}
+	
+	@Transactional(readOnly = true)
+	@Override
+	public List<Gene> getAllGenes()
+	{
+		getSession();
+		@SuppressWarnings("unchecked")
+		List<Gene> genes = session.createQuery("from Gene").getResultList();
+		return genes;
+	}
+	
+	@Override
+	public void addGenes(List<String> symbols)
+	{
+		if (this.session == null || !this.session.isOpen())
+		{
+			this.session = sessionFactory.openSession();
+		}
+		this.session.setJdbcBatchSize(500);
+//		this.session.setHibernateFlushMode(FlushMode.COMMIT);
+		Transaction tran = this.session.beginTransaction();
+		int i = 0;
+		for (String symbol : symbols)
+		{
+			i++;
+			Gene gene = new Gene();
+			gene.setSymbol(symbol);
+//			try
+//			{
+				session.save(gene);
+//			}
+//			catch (Error e)
+//			{
+//				// write out the message, but don't crash.
+//				if (e.getMessage().contains("Duplicate entry"))
+//				{
+//					logger.warn(e.getMessage());
+//				}
+//				else
+//				{
+//					// any other constraint violation should be rethrown...
+//					throw e;
+//				}
+//			}
+			if (i % this.session.getJdbcBatchSize() == 0)
+			{
+				this.session.flush();
+				this.session.clear();
+			}
+		}
+		tran.commit();
+		this.session.close();
 	}
 
 }
