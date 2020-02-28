ee93c7f30cf79ebcf53f2bb56cb21af859cefecc
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2020-02-28 17:07:10 -0500

    new protein ID type for EntrezGene

--- src/main/java/org/reactome/idg/model/ProteinIdentifierType.java
+++ src/main/java/org/reactome/idg/model/ProteinIdentifierType.java
@@ -2,5 +2,5 @@ package org.reactome.idg.model;
 
 public enum ProteinIdentifierType
 {
-	STRINGDB, UNIPROT_GENE, UNIPROT_ACCESSION, SGD
+	STRINGDB, UNIPROT_GENE, UNIPROT_ACCESSION, SGD, ENTREZ_GENE
 }
