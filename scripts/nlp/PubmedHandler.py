ee69380ffc9874a3499488c3c62ae7d6b7a63407
Guanming Wu <wug@rws07890.ohsu.edu>
2022-02-22 17:21:09 -0800

    Updated for running at MacPro

--- scripts/nlp/PubmedHandler.py
+++ scripts/nlp/PubmedHandler.py
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 DOWNLOAD_URL = "https://ftp.ncbi.nlm.nih.gov/pubmed/baseline/pubmed22n{:04d}.xml.gz"
 OUT_DIR = "../../results/impact_analysis/pubmed_baseline"
 MAX_ID = 1114 # The largest number of 2022 annual baseline of pubmed
-MAX_WORKER = 4
+MAX_WORKER = 12
 
 
 def ensure_out_dir(dir_name: str):
@@ -34,7 +34,6 @@ def dowload_link(dir_name: str,
     """
     logger.info("Download {}...".format(link))
     download_path = Path(dir_name + "/" + os.path.basename(link))
-    print(download_path)
     with urlopen(link) as xml, download_path.open('wb') as f:
         f.write(xml.read())
     logger.info('Download {} Done.'.format(link))
@@ -43,7 +42,7 @@ def dowload_link(dir_name: str,
 def download():
     ensure_out_dir(OUT_DIR)
     # Get links for download
-    links = [DOWNLOAD_URL.format(id) for id in range(1, MAX_ID + 1) if id % 200 == 0]
+    links = [DOWNLOAD_URL.format(id) for id in range(1, MAX_ID + 1)]
     # dowload_link(OUT_DIR, links[0])
     with ThreadPoolExecutor(max_workers=MAX_WORKER) as executor:
         try:
