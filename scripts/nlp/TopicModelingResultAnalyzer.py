0468be28ab2bde87af7b2af0eed5d8512cd81376
guanmingwu <guanmingwu@gmail.com>
2022-08-23 17:36:58 -0700

    new notebook for analyzing scRNAseq correlation

--- scripts/nlp/TopicModelingResultAnalyzer.py
+++ scripts/nlp/TopicModelingResultAnalyzer.py
@@ -1,6 +1,7 @@
 # This script is used to process the topic analysis results.
 import psutil
 from sentence_transformers import util
+from scipy.stats import mannwhitneyu
 import numpy as np
 import pandas as pd
 from typing import Tuple
@@ -14,7 +15,7 @@ import time
 
 MINIMUM_SCORE = 1.0e-22
 RANDOM_STATE = 123456
-
+RESULT_DIR = '../../results/impact_analysis/nlp_files/'
 
 def calculate_cosine_similiarity(abstract_embedding: np.ndarray,
                                  pathway2embedding: dict,
@@ -53,7 +54,7 @@ def calculate_cor_impact_cosine(pathway2cosine: dict,
         if impact_score.empty:
             continue
         impact_score = impact_score.iloc[0]
-        if impact_score < MINIMUM_SCORE:
+        if ('FDR' != col_name) and (impact_score < MINIMUM_SCORE):
             continue  # Set a minimum
         if col_name == 'FDR':
             impact_score = -math.log10(impact_score)
@@ -219,57 +220,57 @@ def plot_permutation_results(real_data_file_pattern: str,
         # Load real data
         real_data_file_name = (real_data_file_pattern + "_{}.txt").format(i)
         cor_df_tmp = pd.read_csv(real_data_file_name, sep="\t")
+        cor_df_tmp.set_index('Gene', inplace=True, drop=False)
+        _attach_devlevel_annotated(cor_df_tmp)
         cor_df_tmp['Batch'] = "Real_{}".format(i)
+        cor_df_tmp['Type'] = 'Real'
         # Load permutation data
         random_data_file_name = (random_data_file_pattern + "_{}.txt").format(i)
         random_cor_df_tmp = pd.read_csv(random_data_file_name, sep="\t")
+        random_cor_df_tmp.set_index('Gene', inplace=True, drop=False)
+        _attach_devlevel_annotated(random_cor_df_tmp)
         random_cor_df_tmp['Batch'] = 'Random_{}'.format(i)
+        random_cor_df_tmp['Type'] = 'Random'
         # Merge these two data together
         if cor_df is None:
             cor_df = cor_df_tmp
         else:
             cor_df = pd.concat([cor_df, cor_df_tmp], axis=0)
         cor_df = pd.concat([cor_df, random_cor_df_tmp], axis=0)
-    print(cor_df)
+    # print(cor_df)
     fig = px.violin(cor_df,
                     y = 'Pearson',
                     x='Batch',
+                    color='Tdark',
                     box = True,
                     points=None,
                     hover_data=cor_df.columns)
     fig.write_html(real_data_file_pattern + ".violin.html")
+    # May produce pdf file directly
+    # fig.write_image('test.pdf')
+
     return cor_df
 
 
 def plot_cor_batch_results(file_name: str,
                            col_index: int = 1,
                            color_col_name: str = 'Tdark',
-                           need_violin_plot: bool = False):
+                           need_violin_plot: bool = False,
+                           mannwhitneyu_test: bool=False):
     """
     This methos is used to analyze the results from batch correlation analysis between impact scores
     and pubmed abstract similarity scores.
     :param file_name:
     :param out_file_name:
     :param col_index:
+    :param mannwhitneyu_test: perform mannwhitneyu test if true. This works if need_violin_plot = True.
     :return:
     """
+    print("Analyzing {}...".format(file_name))
     cor_df = pd.read_csv(file_name, sep='\t')
     cor_df.set_index('Gene', inplace=True, drop=False)
     cor_df['-Log10(p-value)'] = cor_df.iloc[:, col_index + 1].map(lambda x : -math.log10(x))
-    # For gene dev levels
-    gene_dev_df_file = '../../src/main/resources/UniProtGeneDevLevelsTypes_100721.txt'
-    gene_dev_df = pd.read_csv(gene_dev_df_file, sep='\t')
-    # There are some dupicated gene names for the same UniProt accessions
-    gene_dev_df.drop_duplicates('GeneName', inplace=True)
-    gene_dev_df.set_index('GeneName', inplace=True)
-    cor_df['DevLevel'] = gene_dev_df.loc[cor_df.index]['DevLevel']
-    cor_df['Tdark'] = cor_df['DevLevel'].map(lambda x : x == 'Tdark')
-    # Check if genes are annotated in Reactome
-    reactome_gene_file_name = '../../results/impact_analysis/ReactomeGenes_Ver_77.txt'
-    reactome_gene_file = open(reactome_gene_file_name, 'r')
-    reactome_genes = [line.strip() for line in reactome_gene_file.readlines()]
-    reactome_gene_file.close()
-    cor_df['Annotated'] = cor_df['Gene'].map(lambda g : g in reactome_genes)
+    _attach_devlevel_annotated(cor_df)
     # scatter between correlation and p-values
     fig = px.scatter(cor_df,
                      x=cor_df.columns[col_index],
@@ -279,22 +280,67 @@ def plot_cor_batch_results(file_name: str,
     fig.write_html(file_name + "." + color_col_name.lower() + ".scatter.html")
     if need_violin_plot:
         fig = px.violin(cor_df,
-                        y = 'Pearson',
+                        y = cor_df.columns[col_index],
                         x='Annotated' if color_col_name == 'Tdark' else 'Tdark',
                         color=color_col_name,
                         box = True,
                         points='all',
                         hover_data=cor_df.columns)
         fig.write_html(file_name + "." + color_col_name.lower() + ".violin.html")
+        if mannwhitneyu_test:
+            # Perform Mann-Whitney U rank test
+            if color_col_name == 'Tdark':
+                # Perform test between Annotated true and false
+                # Check true genes first
+                x = cor_df[(cor_df['Annotated'] == True) & (cor_df['Tdark'] == True)].iloc[:, col_index]
+                y = cor_df[(cor_df['Annotated'] == True) & (cor_df['Tdark'] == False)].iloc[:, col_index]
+                test_results = mannwhitneyu(x, y) # Use default for two side test
+                print('Annotated: TDark vs Non-Tdark, {}.'.format(test_results))
+                # Check false genes
+                x = cor_df[(cor_df['Annotated'] == False) & (cor_df['Tdark'] == True)].iloc[:, col_index]
+                y = cor_df[(cor_df['Annotated'] == False) & (cor_df['Tdark'] == False)].iloc[:, col_index]
+                test_results = mannwhitneyu(x, y)  # Use default for two side test
+                print('Non-Annotated: TDark vs Non-Tdark, {}.'.format(test_results))
+            elif color_col_name == 'Annotated':
+                # Perform test between TDark true and false
+                # Check true genes first
+                x = cor_df[(cor_df['Tdark'] == True) & (cor_df['Annotated'] == True)].iloc[:, col_index]
+                y = cor_df[(cor_df['Tdark'] == True) & (cor_df['Annotated'] == False)].iloc[:, col_index]
+                test_results = mannwhitneyu(x, y)  # Use default for two side test
+                print('TDark: Annotated vs Non-Annotated, {}.'.format(test_results))
+                # Check false genes
+                x = cor_df[(cor_df['Tdark'] == False) & (cor_df['Annotated'] == True)].iloc[:, col_index]
+                y = cor_df[(cor_df['Tdark'] == False) & (cor_df['Annotated'] == False)].iloc[:, col_index]
+                test_results = mannwhitneyu(x, y)  # Use default for two side test
+                print('Non-TDark: Annotated vs Non-Annotated, {}.'.format(test_results))
     return cor_df
 
 
-if __name__ == '__main__':
-    dir_name = '../../results/impact_analysis/nlp_files/'
-    file_name = dir_name + 'FDR_impact_pubmed_score_cor_random_04242022_0.txt'
-    # file_name = dir_name + 'Average_Inhibition_impact_pubmed_score_cor_random_04242022_0.txt'
-    plot_cor_batch_results(file_name, need_violin_plot=False, color_col_name='Annotated')
-    # plot_cor_batch_results(file_name, need_violin_plot=True, color_col_name='Tdark')
+def _attach_devlevel_annotated(cor_df):
+    # For gene dev levels
+    gene_dev_df_file = '../../src/main/resources/UniProtGeneDevLevelsTypes_100721.txt'
+    gene_dev_df = pd.read_csv(gene_dev_df_file, sep='\t')
+    # There are some dupicated gene names for the same UniProt accessions
+    gene_dev_df.drop_duplicates('GeneName', inplace=True)
+    gene_dev_df.set_index('GeneName', inplace=True)
+    cor_df['DevLevel'] = gene_dev_df.loc[cor_df.index]['DevLevel']
+    cor_df['Tdark'] = cor_df['DevLevel'].map(lambda x: x == 'Tdark')
+    # Check if genes are annotated in Reactome
+    reactome_gene_file_name = '../../results/impact_analysis/ReactomeGenes_Ver_77.txt'
+    reactome_gene_file = open(reactome_gene_file_name, 'r')
+    reactome_genes = [line.strip() for line in reactome_gene_file.readlines()]
+    reactome_gene_file.close()
+    cor_df['Annotated'] = cor_df['Gene'].map(lambda g: g in reactome_genes)
+
+#
+# if __name__ == '__main__':
+#     dir_name = '../../results/impact_analysis/nlp_files/'
+#     # file_name = dir_name + 'FDR_impact_pubmed_score_cor_random_04242022_0.txt'
+#     file_name = dir_name + 'FDR_impact_pubmed_score_cor_04242022_0.txt'
+#     # file_name = dir_name + 'Average_Inhibition_impact_pubmed_score_cor_random_04242022_0.txt'
+#     # file_name = dir_name + 'FDR_impact_pubmed_score_cor_04272022_0.txt'
+#     plot_cor_batch_results(file_name, need_violin_plot=True, color_col_name='Annotated', mannwhitneyu_test=True)
+#     plot_cor_batch_results(file_name, need_violin_plot=True, color_col_name='Tdark', mannwhitneyu_test=True)
 
 
 @ray.remote
